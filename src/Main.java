d8fe102a2c2965262a45e1eb455967fdcd8c557e
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-11-22 13:19:30 -0300

    feat: testing with news singular functions

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import handlerText.ReaderText;
-import handlerText.RegexTextProcessing;
+import handlerText.normalizedWords.RegexTextProcessing;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void printToWordSeparator(String[] text){
+    public static void printToWordSeparator(String[] text) throws IOException {
         for(int i = 0; i < text.length; i++){
-            String[] wordsText = RegexTextProcessing.separatorWord(text[i]);
+            String[] wordsText = RegexTextProcessing.separtorWord(text[i], "src/data/restric_words.txt");
 
             System.out.println(Arrays.toString(wordsText));
 
@@ -17,10 +18,10 @@ public class Main {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        String[] text1 = ReaderText.read("src/data/texto1");
-        String[] text2 = ReaderText.read("src/data/texto2");
-        String[] text3 = ReaderText.read("src/data/texto3");
+    public static void main(String[] args) throws IOException {
+        String[] text1 = ReaderText.read("src/data/texts/texto1");
+        String[] text2 = ReaderText.read("src/data/texts/texto2");
+        String[] text3 = ReaderText.read("src/data/texts/texto3");
 
         printToWordSeparator(text1);
         printToWordSeparator(text2);
