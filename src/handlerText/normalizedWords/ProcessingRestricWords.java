9f5eb58926c7b65e19133d4a9c6a90da1f93b981
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-11-23 04:10:09 -0300

    refactory: processing word remove data struct default java and remove functions normalized of RegexTextProcessing

--- src/handlerText/normalizedWords/ProcessingRestricWords.java
+++ src/handlerText/normalizedWords/ProcessingRestricWords.java
@@ -1,28 +1,30 @@
 package handlerText.normalizedWords;
 
+import structures.HashTable;
+import structures.Word;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class ProcessingRestricWords {
 
-    // TODO(mudar para estrutura de dados propria)
-    private static final Set<String> INVARIABLES = new HashSet<>();
+    private static final HashTable INVARIABLES = new HashTable();
 
     public static void generated(String file) throws IOException {
         Path path = Paths.get(file);
         String[] content = Files.readAllLines(path).toArray(new String[0]);
 
-        // TODO(mudar para estrutura de dados propria)
-        INVARIABLES.addAll(Arrays.stream(content).toList());
+        Arrays.stream(content).forEach( word -> {
+            INVARIABLES.insert(NomalizedWord.normalizeAccents(word), 0);
+        });
     }
 
     public static Boolean verify(String word){
-        return INVARIABLES.contains(word);
+        Word data = INVARIABLES.search(word);
+        return data != null;
     }
 
 }
